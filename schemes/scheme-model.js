665ae37938d5a0f9489f97a379e76a23ec6caea3
Wangdi Tenzing <wangdi457@gmail.com>
2020-04-16 19:02:37 -0400

    completed scheme-model MVP Reached

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,10 +1,6 @@
 const db= require ("../data/knexconnect")
 
-module.exports={
-find,
-findById,
 
-}
 
 function find(){
  return db('schemes')
@@ -12,5 +8,39 @@ function find(){
 }
 
 function findById(id){
-    db('schemes').where({id}).first()
+    return db('schemes').where({id}).first()
 }
+
+function  findSteps(scheme_id){
+    return db("steps as p")
+    .join('schemes as s', 'p.scheme_id', 's.id')
+    .select ("p.id","s.scheme_name", "p.step_number","p.instructions")
+    .where({scheme_id})
+   }
+   
+function add(schemes){
+    return db('schemes').insert(schemes)
+}
+
+function update(changes,id){
+    return db('schemes')
+    .where({id})
+    .update(changes)
+    
+}
+
+ function remove(id){
+     return db('schemes')
+     .where({id})
+     .del()
+     
+ }
+
+module.exports={
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+    }
